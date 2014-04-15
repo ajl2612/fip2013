6a9726de9d5bcf3b63da5c6580a58dcc64cbc189
Andrew Lyne <ajl2612@gmail.com>
2014-04-14 20:47:24 -0400

    Changes server IP to function on RIT network. Added base C++ implementation of socket communication

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import socket
 host = socket.gethostname()
 print(host)
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.bind(('', 8091))
+serversocket.bind(('', 8090))
 serversocket.listen(5) # become a server socket, maximum 5 connections
 
 print("Awaiting packages")
