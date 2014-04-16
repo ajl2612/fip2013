7b849736af5ef0e3b77e97275cdcca2917b17856
Andrew Lyne <ajl2612@gmail.com>
2014-04-15 22:26:22 -0400

    removed gohst files

--- README.md
+++
@@ -1,4 +0,0 @@
-fip2013
-=======
-
-Freshman Imaging Science Project 2013 - Intelligent Multi Camera Video Chat
--- client.py
+++
@@ -1,5 +0,0 @@
-import socket
-clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientsocket.connect(('129.21.58.247', 8090))
-message = 'Banana Pancakes'
-clientsocket.send(message.encode())
