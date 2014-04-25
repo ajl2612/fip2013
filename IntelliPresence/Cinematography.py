e98e25df1d1dab958ced9c15e50cef3908af4d60
MorganCabral <morgan.cabral@gmail.com>
2014-04-24 22:09:08 -0400

    Modified Cinematography so that it takes in the list of cameras and motors as parameters. Dependency Injection is an object oriented programming technique where a class takes in all of it's dependencies instead of creating them internally. The reason this is useful is that it makes testing much easier down the road and also forces you to write your code to implement a specific interface.

--- IntelliPresence/Cinematography.py
+++ IntelliPresence/Cinematography.py
@@ -5,18 +5,19 @@ at CIS, RIT
 Authors: Noah Kram, Anna Dining
 """
 #import atem_control as bm
-from threadServer import NetwrokThread
+from threadServer import NetworkThread
 from Camera import Camera
 import socket
 
 class Cinematography:
-    
-	defaultAngle = 2
-	def __init__(self):
+	"""
+        A collection of helper methods for working with cameras and motors.
+        """
+	def __init__(self, cam_list, motor_list, default_camera_angle = 2):
 		"""
 		Initializes cinematography structure, allows access to tracking data
 	
-		Note: self.mcsCams is a Tuple of the 4 mcs cameras. They are in order
+		Note: self.mcsCams is a list of the 4 mcs cameras. They are in order
 		(0, 1, 2, 3) and the values in them are updated continuously by code
 		from MCS. To access values in them, refer to the Camera class
 	
@@ -25,28 +26,18 @@ class Cinematography:
 		"""
 		self.currentCamera = 1
 		self.previousCamera = 1
+                self.default_camera_angle = default_camera_angle
 		self.angles = {1:2, 2:2, 3:2, 4:2}
-		self.motorIPs = {1:('129.21.58.202',8094),2:('129.21.58.202',8094),
-			3:('129.21.58.202',8094), 4:('129.21.58.202',8094)}
+		#self.motorIPs = { 1:('129.21.58.202',8094), 2:('129.21.58.202',8094),
+		#	3:('129.21.58.202',8094), 4:('129.21.58.202',8094)}
 	        
-		cam1 = Camera()
-		cam2 = Camera()
-		cam3 = Camera()
-		cam4 = Camera()
-		mcscams = []
-	
-		mcscams.append(cam1)
-		mcscams.append(cam2)
-		mcscams.append(cam3)
-		mcscams.append(cam4)
-		#self.mcsCams = tuple(mcsCams)
-		self.mcsCams = mcscams
+		self.mcsCams = cam_list
+                self.mcsMotors = motor_list
+
 		thread1 = NetworkThread("", 8091, cam1)
 		thread2 = NetworkThread("", 8092, cam2)
-		thread1.start() # This actually causes the thread to run
+		thread1.start() 
 		thread2.start()
-
-    
         
 	def camera_ranks(self):
 		"""
@@ -54,7 +45,6 @@ class Cinematography:
 		as given by Facial Detection
 		returns a list (1-4) of the rankings by largest area of each mcs cam
 		"""
-
 		tempList = []
 		detectedList = []
 		for c in self.mcsCams:
@@ -73,9 +63,8 @@ class Cinematography:
 			z[i] = index + 1
 		#z.sort(key = lambda x: self.mcsCams.index(tempList[x]))
 		return z
-    
-    
-	def best_angle(self, cam, mcs=False):
+	
+        def best_angle(self, cam, mcs=False):
 		"""
 		Finds the best angle for camera 'cam'
         
@@ -83,7 +72,7 @@ class Cinematography:
 		mcs = (bool) if method should run based on mcs input. False by default
 		"""
 		#IR_code.getAngle or whatever they use
-		bestAngle = defaultAngle
+		bestAngle = self.default_camera_angle
 		if not mcs:#Fix following part
 			if angle >= 0 and angle < 30:
 				newPanAngle= 1            
@@ -140,19 +129,7 @@ class Cinematography:
 				message = 'True:Right:1'
 			else:
 				pass
-		print('Sending packet')
+		print 'Sending packet'
 		sock.send(message.encode())
 		self.angles[cam] = position
-	    
 
-#        
-#if __name__ == '__main__':
-#
-#    cine = Cinematography()
-#    cine.mcsCams[0] = Camera(0,0,0,0,False)#, 'cam1')
-#    cine.mcsCams[1] = Camera(0, 25, 35, 60)#, name='cam2')
-#    cine.mcsCams[2] = Camera(0, 60, 600, 150)#, name='cam3')
-#    cine.mcsCams[3] = Camera(30, 20, 300, 35)#,False, name='cam4')
-#
-#    #print cine.camera_ranks()    
-#    cine.pan_camera(1,2)
