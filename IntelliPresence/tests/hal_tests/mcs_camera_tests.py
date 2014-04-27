fc2540e5ab9c64d4f443b00b524378762e733613
MorganCabral <morgan.cabral@gmail.com>
2014-04-26 22:38:00 -0400

    Fixed up the test.

--- IntelliPresence/tests/hal_tests/mcs_camera_tests.py
+++ IntelliPresence/tests/hal_tests/mcs_camera_tests.py
@@ -3,5 +3,10 @@ from intellipresence.hal.mcs_camera import MCSCamera
 def updateData_test():
     # Note, move this into a unit test later
     cam = MCSCamera( 0,0,0,0,"Red2", 0, False)
-    cam.updateData( 5,5, 10, 20, True )
-    assert False
+    cam.updateData( 5, 5, 10, 20, True )
+    assert cam.getPoint1()[0] == 5
+    assert cam.getPoint1()[1] == 5
+    assert cam.getPoint2()[0] == 15
+    assert cam.getPoint2()[1] == 25
+    assert cam.isFace()
+
