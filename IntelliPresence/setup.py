22026cb3244fd7836a84ba40126fd6c4a5faab18
MorganCabral <morgan.cabral@gmail.com>
2014-04-29 01:54:04 -0400

    Updated the setup.py file to use the python3 version of the netifaces library.

--- IntelliPresence/setup.py
+++ IntelliPresence/setup.py
@@ -7,7 +7,7 @@ setup(
     project_url="https://github.com/ajl2612/fip2013",
     author="Freshman Imaging Team",
     license="MIT",
-    install_requires=['nose', 'netifaces'],
+    install_requires=['nose', 'netifaces_py3'],
     entry_points={
         "console_scripts": [
             "tantive_iv=intellipresence:main",
