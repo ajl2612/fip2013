b3898e1b22e3e55cb360441c937afbf3a8967618
MorganCabral <morgan.cabral@gmail.com>
2014-04-28 21:33:13 -0400

    Added ATEM related values to the configuration.

---
+++ IntelliPresence/intellipresence/config.py
@@ -0,0 +1,9 @@
+# Configuration for the ATEM.
+# adapter refers to the physical adapter that the ATEM is attached to (ie. eth1). 
+# port is the port that will be used for the socket that is created at that address.
+# atem_addr is the address of the ATEM on the network that adapter is attached to. 
+atem = {
+    "adapter": "en2",
+    "port": "9910",
+    "atem_addr": "192.168.10.240",
+}
